aae318001cda77209d07d232a76d81dcc2e8ba5e
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-09-21 02:07:10 -0300

    Create Fenwick Tree.cpp

---
+++ Fenwick Tree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int BIT[1000], a[1000], n;
+void update(int x, int val)
+{
+      for(; x <= n; x += x&-x)
+        BIT[x] += val;
+}
+int query(int x)
+{
+     int sum = 0;
+     for(; x > 0; x -= x&-x)
+        sum += BIT[x];
+     return sum;
+}
