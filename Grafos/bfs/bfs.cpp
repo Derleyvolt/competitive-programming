713cd0dfe6c778cb1a8eec5fc4de170cabe537e4
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-10-15 13:21:46 -0300

    Update bfs.cpp

--- Grafos/bfs/bfs.cpp
+++ Grafos/bfs/bfs.cpp
@@ -2,7 +2,7 @@
 
 using namespace std; 
 
-const int MAXN = 1000;
+const int MAXN = 1010;
 
 int n;
 vector<int> adj[MAXN];
@@ -10,7 +10,6 @@ vector<bool> vis;
 
 void bfs(int s) {
     queue<int> q; q.push(s);
-    vis = vector<bool>(n+1);
     vis[s] = 1;
     while(not q.empty()) {
         int r = q.front(); q.pop();
@@ -25,5 +24,13 @@ void bfs(int s) {
 
 int main() {
     ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
+    cin.tie(NULL);
+    int m; cin >> n >> m;
+    vis.resize(n+1); // no caso de ser 1-indexed
+    while(m--) {
+        int a, b; cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    bfs(1);
 }
