1a945a0caa7497f03bd793765b66e1cd8ed7f9db
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-10-19 07:37:59 -0300

    Update inverter_número.cpp

--- secundários/inverter_número.cpp
+++ secundários/inverter_número.cpp
@@ -2,12 +2,13 @@
 
 using namespace std; 
 
-*/
+/*
    O número 347, por exemplo, em notação posicional é escrito assim: (3 * 10 ^ 2) + (4 * 10 ^ 1) + (7 * 10 ^ 0).
    Dessa forma, fica fácil perceber que quando dividimos o número pela base, o resto sempre será o termo que é menor que a própria base, ou seja,
    o resto sempre será a unidade. Os outros termos sempre serão maiores e múltiplos da base.
    Portanto, basta pegarmos sempre o resto do número original e multiplicarmos por 10 enquanto, só não podemos multiplicar por 10 quando pegarmos o
-   último dígito do número original, o 3, nesse caso, pois ele vai ficar como unidade; para fazermos isso basta 
+   último dígito do número original, o 3, nesse caso, pois ele vai ficar como unidade; para fazermos isso basta não multiplicarmos pela base o último dígito
+   ou simplesmente omitirmos a primeira multiplicação pela base, que é o que iremos fazer.
 */
 
 int reverse_n(int n) {
@@ -15,7 +16,7 @@ int reverse_n(int n) {
     while(n) {
         int q = n % 10;
         n /= 10;
-        r *= 10; 
+        r *= 10; // multiplicando por 0, ou seja, omitindo a primeira multiplicação pela base.
         r += q;
     }
     return r;
@@ -24,6 +25,6 @@ int reverse_n(int n) {
 int main() {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
-    cout << reverse_n(12) << endl;
+    cout << reverse_n(347) << endl;
     return 0;
 }
