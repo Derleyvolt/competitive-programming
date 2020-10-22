96169b98e183545e5de2036a08d8eee6872df21b
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-10-22 20:53:30 -0300

    Update ler_até_newline.cpp

--- ler_até_newline.cpp
+++ ler_até_newline.cpp
@@ -6,6 +6,8 @@ int main() {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
     freopen("in.txt", "r", stdin);
+    // int b; cin >> b;  // 
+    // cin.ignore();     // necessário onde será lido um input da maneira usual e depois usarmos getline..
     string line;
     getline(cin,line);       // ler todos os caracteres do user input buffer no cin
     istringstream iss(line); // essa classe se encarrega de me passar os dados
