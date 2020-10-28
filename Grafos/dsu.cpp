c98e56aa28ee0ef7ed74b8f0cb78c3d4afc5b6af
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-10-28 19:15:08 -0300

    Update dsu.cpp

--- Grafos/dsu.cpp
+++ Grafos/dsu.cpp
@@ -2,20 +2,20 @@
 
 using namespace std; 
 
-const int maxn = 1000;
+const int maxn = 1001;
 
-int p[maxn], s[maxn];
+int parent[maxn], s[maxn];
 
 void init(int n) {
     for(int i = 0; i < n; i++) {
-        p[i] = i;
-        h[i] = 1;
+        parent[i] = i;
+        s[i] = 1;
     }
 }
 
 int root(int u) {
-    if(u == p[u]) return u;
-    return p[u] = root(p[u]); // path compression
+    if(u == parent[u]) return u;
+    return parent[u] = root(parent[u]); // path compression
 }
 
 void join(int u, int v) {
@@ -23,21 +23,21 @@ void join(int u, int v) {
     v = root(v);
     if(u == v) return; // checa se dois vértices pertencem ao mesmo conjunto disjunto
     if(s[v] >= s[u]) swap(v, u);
-    p[v] = u;
-    s[u] += s[v];
+    parent[v] = u;
+    s[u] += s[v]; // perceba que só os representantes de cada conjunto disjunto é que vão guardar a informação do tamanho do conjunto.
 
 /*
     o código acima faz exatamente a mesma coisa que o código abaixo, só é menor..
 
     u = root(u);
     v = root(v);
-    
+    if(u == v) return;
     if(s[v] >= s[u]) {
-        p[u] = v;
+        parent[u] = v;
         s[v] += s[u];
     }
     else {
-        p[v] = u;
+        parent[v] = u;
         s[u] += s[v];
     }
 */
@@ -47,6 +47,11 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n = 7;
-    init(n);
+    init(n+1);
+    join(1, 2);
+    join(3, 6);
+    join(6, 2);
+    cout << root(1) << " " << root(6) << endl;
+    cout << s[root(1)] << " " << s[root(5)] << endl;
     return 0;
 }
