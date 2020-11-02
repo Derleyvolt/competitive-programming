ab99e1a7bfe5f6aa1e3fb01029161451f0cf8fb1
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-11-02 01:42:32 -0300

    Update dsu.cpp

--- Grafos/dsu.cpp
+++ Grafos/dsu.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 const int maxn = 1001;
 
-int parent[maxn], s[maxn];
+int parent[maxn];
+int s[maxn]; // vetor usado na otimização pelo tamanho dos conjuntos
 
 void init(int n) {
     for(int i = 0; i < n; i++) {
@@ -22,7 +23,7 @@ int root(int u) {
 void join(int u, int v) {
     u = root(u);
     v = root(v);
-    if(u == v) return; // checa se dois vértices pertencem ao mesmo conjunto disjunto
+    if(u == v) return; // checa se dois vértices pertencem ao mesmo conjunto.. necessário, sem ele o conjunto "dobraria" de tamanho se u e v fossem do mesmo conjunto
     if(s[v] >= s[u]) swap(v, u);
     parent[v] = u;
     s[u] += s[v]; // perceba que só os representantes de cada conjunto disjunto é que vão guardar a informação do tamanho do conjunto.
@@ -44,6 +45,13 @@ void join(int u, int v) {
 */
 }
 
+// como a otimização pelo tamanho dos conjuntos nem sempre é necessária, pode-se usar isse código com frequência..
+void join(int u, int v) {
+    u = root(u);
+    v = root(v);
+    parent[u] = v;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -52,7 +60,7 @@ int main() {
     join(1, 2);
     join(3, 6);
     join(6, 2);
-    cout << root(1) << " " << root(6) << endl;
-    cout << s[root(1)] << " " << s[root(5)] << endl;
+    cout << root(1) << " " << root(6) << endl; // retorna o representante do conjunto que contém 1 e do conjunto que contém 6.
+    cout << s[root(1)] << " " << s[root(5)] << endl; // retorna o tamanho dos conjuntos que contêm o elemento 1 e 5
     return 0;
 }
