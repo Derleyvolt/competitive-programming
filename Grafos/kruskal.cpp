c217e8a4a23c514443a6a02bf30e3feccc5c8523
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-11-03 03:27:05 -0300

    Update kruskal.cpp

--- Grafos/kruskal.cpp
+++ Grafos/kruskal.cpp
@@ -24,20 +24,36 @@ void join(int u, int v) {
     parent[v] = u;
 }
 
-array<int, 3> arr[maxn];
+array<int, 3> arr[maxn]; // u, v e w
 
 int mst() {
     int ans = 0;
 
-    sort(arr, arr + m, [](auto a, auto b) {
-        return a[2] > b[2];
-    });
+    sort(arr, arr + m); // ordenada as arestas em função dos pesos
 
+/*   
+    considere v como sendo o número de vértices.
+
+    o j é usado numa otimização que verifica se o número de arestas encontradas tem tamanho v-1.  
+    essa otimização só funciona em grafos com 1 componente.. mas não há problemas em usar em grafos com várias componentes conexas,
+    a otimização não vai funcionar mas não vai atrapalhar.
+    
+    como o número exato de arestas pertencentes a (minimum) spanning tree é v-1, ele para quando o número de arestas obtidas for esse.
+    
     for(int i = 0, j = 0; i < m and j < n-1; i++) {
         auto [u, v, w] = arr[i];
 
         if(root(u) != root(v)) {
             j++;
+            ans += w;
+            join(u, v);
+        }
+    }
+*/  
+    for(int i = 0; i < m; i++) {
+        auto [u, v, w] = arr[i];
+
+        if(root(u) != root(v)) {
             ans += w;
             join(u, v);
         }
