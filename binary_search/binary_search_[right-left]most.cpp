f0239d6a6aaea6ed0f74c37b42fd9d5b3fa82691
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-11-30 01:14:40 -0300

    Update and rename binary_search_rightmost.cpp to binary_search_[right-left]most.cpp

--- binary_search/binary_search_[right-left]most.cpp
+++ binary_search/binary_search_[right-left]most.cpp
@@ -6,7 +6,7 @@ const int maxn = 1e4 + 10;
 
 int arr[maxn], n;
 
-int bb_2(int v) {
+int binary_search_rightmost(int v) {
     int l = 0, r = n-1;
     int ans = -1; // variável que guardará o primeiro cara que é maior que o valor buscado
     while(l <= r) {
@@ -23,6 +23,23 @@ int bb_2(int v) {
     return ans;
 }
 
+int binary_search_leftmost(int v) {
+    int l = 0, r = n-1;
+    int ans = -1; // variável que guardará o primeiro cara que é menor que o valor buscado
+    while(l <= r) {
+        int m = l + (r - l) / 2;
+        if(arr[m] == v)
+            return m;
+        if(arr[m] < v) {
+            ans = m; // guarda sempre o valor mais próximo de v, que é menor que v
+            l = m + 1;
+        }
+        else
+            r = m - 1;
+    }
+    return ans;
+}
+
 int main() {
     n = 5;
     arr[0] = 1; 
@@ -30,6 +47,8 @@ int main() {
     arr[2] = 3; 
     arr[3] = 5;  
     arr[4] = 6;
+    
+    // A explicação daqui é destinado à função binary_search_rightmost.. a explicação pra função binary_search_leftmost é análoga.
     
     // queremos buscar pelo valor 3.. então colocamos pra encontrar o valor 4, pois o algoritmo vai buscar
     // pelo valor imediato que for >= 4.. ou seja, se o 4 estiver no array, o 4 será encontrado, senão, o próximo
