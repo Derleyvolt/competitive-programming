792339d786fef151f202d445c8ea705691541491
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-11-30 00:31:37 -0300

    Update and rename binary_search_leftmost_rightmost.cpp to binary_search_rightmost.cpp

---
+++ binary_search/binary_search_rightmost.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+const int maxn = 1e4 + 10;
+
+int arr[maxn], n;
+
+int bb_2(int v) {
+    int l = 0, r = n-1;
+    int ans = -1; // variável que guardará o primeiro cara que é maior que o valor buscado
+    while(l <= r) {
+        int m = l + (r - l) / 2;
+        if(arr[m] == v)
+            return v;
+        if(arr[m] > v) {
+            ans = m; // guarda sempre o valor mais próximo de v, que é maior que v
+            r = m - 1;
+        }
+        else
+            l = m + 1;
+    }
+    return ans;
+}
+
+int main() {
+    n = 5;
+    arr[0] = 1; 
+    arr[1] = 2;
+    arr[2] = 3; 
+    arr[3] = 5;  
+    arr[4] = 6;
+    
+    // queremos buscar pelo valor 3.. então colocamos pra encontrar o valor 4, pois o algoritmo vai buscar
+    // pelo valor imediato que for >= 4.. ou seja, se o 4 estiver no array, o 4 será encontrado, senão, o próximo
+    // valor maior que ele será encontrado. E com isso, se o valor 3 estiver no array, ele deve estar um índice a menos
+    // que o valor encontrado.
+    int index = bb_2(4) - 1;          
+    if(index >= 0 and arr[index] == 3)
+        cout << arr[index] << endl;
+    else
+        cout << -1 << endl;
+    return 0;
+}
