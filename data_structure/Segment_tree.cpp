6b53a79c8952104881dd2a958683e3fc7f91532a
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-12-25 06:17:45 -0300

    Update Segment_tree.cpp

--- data_structure/Segment_tree.cpp
+++ data_structure/Segment_tree.cpp
@@ -1,117 +1,74 @@
-
-#include<bits/stdc++.h>
-#define INF 0x3f3f3f3f
-#define MAX (1<<20)
-#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define esq(x) (2*(x))
-#define dir(x) (2*(x)+1)
-#define OUT -INF
+#include <bits/stdc++.h>
 
 using namespace std;
 
-/*
-query(l,r) : soma do intevalo A[L..R]
-update(idx, val):  faz A[idx] = VAl
-*/
-
 int a[MAX]; // array original
-int tsum[MAX];
+int seg[MAX];
 
-int n; //tamanho de A[]
-
-int comb(int x, int y){
-    return x+y;
-}
-
-void build(int u, int l, int r){// u é o indice do nó n árvore, e armzzena o segmento A[i..j]
-
-    if (l == r){
+void build(int u, int l, int r) {// u é o indice do nó n árvore, e armzzena o segmento A[i..j]
+    if (l == r) {
         tsum[u] = a[l];
         return;
     }
 
-    int md = (l+r)/2;
+    int md = (l + r) / 2;
 
-    build(esq(u), l, md); // controi sub arvore esquerda
-    build(dir(u), md+1, r); // controi sub arvore direita
-    tsum[u] = comb(tsum[esq(u)], tsum[dir(u)]);
+    build(u * 2, l, md); // controi sub arvore esquerda
+    build(u * 2 + 1, md + 1, r); // controi sub arvore direita
+    tsum[u] = comb(tsum[u * 2], tsum[u * 2 + 1]);
 }
 
-void update(int u, int l, int r, int idx, int val){
-
-    if (l == r){
+void update(int u, int l, int r, int idx, int val) {
+    if (l == r) {
         tsum[u] = val;
         return;
     }
 
-    int md = (l + r)/2;
+    int m = (l + r) / 2;
 
-    if (l<=idx && idx<=md) update(esq(u), l, md, idx, val);
-    if (md<idx && idx<=r) update(dir(u), md+1, r, idx, val);
-
-    tsum[u] = comb(tsum[esq(u)], tsum[dir(u)]);
-}
-
-void update(int u, int l, int r, int pos, int val) {
-    if (l == r)
-        t[u] = val, return;
-    
-    int m = (l + r) / 2;  
-    if (pos <= m)
-        update(u*2, l, m, pos, val);
+    if (idx <= m)
+        update(esq(u), l, m, idx, val);
     else
-        update(u*2+1, m+1, r, pos, val);
-    t[u] = t[u*2] + t[u*2+1];
+        update(dir(u), m + 1, r, idx, val);
+
+    tsum[u] = comb(tsum[u * 2], tsum[u * 2 + 1]);
 }
 
-int query(int v, int tl, int tr, int l, int r) {
-    if (l > r) 
+int query(int u, int tl, int tr, int l, int r) {
+    if (l > r)
         return 0;
     if (l == tl and r == tr)
-        return t[v];
+        return tsum[u];
+
     int m = (tl + tr) / 2;
-    return sum(v*2, tl, m, l, min(r, m)) + sum(v*2+1, m + 1, tr, max(l, m + 1), r);
-}
-
-int query(int u, int i, int j, int l, int r){
-    if (l<=i && j<=r){
-        return tsum[u]; // [i..i] está completamente dentro de [l..r]
-    }
-    if (j<l || r<i){
-        return OUT; // [i..i] está completamente fora de [l..r]
-    }
-
-    // Caso de escape, default, 'overlap' -> desce na árvore
-    int md = (i+j)/2;
-
-    int ans_esq = query(esq(u), i, md, l, r);
-    int ans_dir = query(dir(u), md+1, j, l, r);
-
-    if (ans_esq == OUT) return ans_dir; 
-    if (ans_dir == OUT) return ans_esq; 
-
-    return tsum[u] = comb(ans_esq, ans_dir); 
+    int a = query(u * 2, tl, m, l, min(r, m));
+    int b = query(u * 2 + 1, m + 1, tr, max(l, m + 1), r);
+    return a + b;
 }
 
 int l, r, idx, val, op;
 
-int main(){_
+int main() {
+    long long aa = -5;
+    cout << aa << endl;
+
+    return 0;
+
+    freopen("in.txt", "r", stdin);
     cin >> n;
 
-    for (int i=1; i<=n; ++i){
+    for (int i = 1; i <= n; ++i)
         cin >> a[i];
-    }
 
     build(1, 1, n);
 
-    while (cin >> op){
-        if (op == 1){
+    while (cin >> op) {
+        if (op == 1) {
             cin >> idx >> val;
             update(1, 1, n, idx, val);
-
             cout << "Atualizou" << endl;
         }
-        if (op == 2){
+        if (op == 2) {
             cin >> l >> r;
             cout << "soma: " << query(1, 1, n, l, r) << endl;
         }
