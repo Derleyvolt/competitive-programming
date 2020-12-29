11a64fc193859ddc3bcaeba4cef8672233b43d1e
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-12-29 03:33:57 -0300

    Create basic_bitmask_tricks.cpp

---
+++ bitmask/basic_bitmask_tricks.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void show_bits(int v, int n, bool f) {
+    if(n == 0)
+        return;
+    show_bits(v>>1, n-1, 0);
+    if(f)  
+        cout << (v&1) << endl;
+    else
+        cout << (v&1);
+}
+
+int union_set(int a, int b) {
+    return a | b; // união dos elementos dos conjuntos
+}
+
+int intersection_set(int a, int b) {
+    return a & b; // o novo conjunto terá 1's apenas onde houver 1's em posições iguais em ambos os conjuntos
+}
+
+int difference(int a, int b) { // a ordem é importante, obviamente..
+    return (a | b) ^ a;
+}
+
+int main() {
+    int a = 12, b = 14; // 1100, 1110
+    show_bits(union_set(a, b), 4);
+    show_bits(intersection_set(a, b), 4);
+    show_bits(difference(a, b), 4);
+    show_bits(difference(b, a), 4); // difference não é comutativa
+    return 0;
+}
