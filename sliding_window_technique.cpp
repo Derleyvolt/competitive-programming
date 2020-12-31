1403366f9585095f29914ac84b3e8cbb43e04845
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2020-12-30 21:31:46 -0300

    Update sliding_window_technique.cpp

--- sliding_window_technique.cpp
+++ sliding_window_technique.cpp
@@ -6,19 +6,19 @@ const int maxn = 1e5 + 10;
 
 int arr[maxn];
 
-bool sliding_window(int w, int n, int v) { // w = tamanho da janela, n = tamanho do array e v = valor a ser buscado
+bool sliding_window(int w, int n, int val) { // w = tamanho da janela, n = tamanho do array e val = valor buscado
     if(w > n) return false;
 
     int ans = 0;
     for(int i = 0; i < w; i++) 
         ans += arr[i];
 
-    if(ans == v) return true;
+    if(ans == val) return true;
 
     for(int i = 0; i < n-w; i++) {
         ans += arr[i + w] - arr[i];
         //assert(cout << ans << endl and i + w <= n-1);
-        if(ans == v) return true;
+        if(ans == val) return true;
     }
     return false;
 }
@@ -27,8 +27,9 @@ int main() {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
     freopen("in.txt", "r", stdin);
-    int n; cin >> n;
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    int length, window, val; cin >> length >> window >> val;
+    for(int i = 0; i < n; i++) 
+        cin >> arr[i];
     cout << boolalpha << sliding_window(2, n, 20) << endl;
     return 0;
 }
