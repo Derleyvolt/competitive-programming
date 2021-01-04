2b182e4b90550cd0ba48b8be1a25c71885077dcd
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2021-01-04 09:31:48 -0300

    Update mx_&_smx.cpp

--- tricks/mx_&_smx.cpp
+++ tricks/mx_&_smx.cpp
@@ -4,25 +4,40 @@ using namespace std;
 
 /*
     É comum nos depararmos com situações onde devemos pegar o maior e o segundo maior elemento de um conjunto de 3 ou 4 elementos..
-    eis aqui minha default-way de fazer isso
+    eis aqui minha default-way de fazer isso. a complexidade do sort não causa overhead por conta da quantidade de elementos, então funciona bem.
 */
 
-#define ii pair<int, int>
+#define ii array<int, 2> // array, diferentemente de pair, não é inicializado quando instanciado na stack..
+#define vi vector<int>
 
-pair<int, int> comb(ii l, ii r) {
-    int a = l.first, b = l.second, c = r.first, d = r.second;
-    auto ans = vector<int>{a, b, c, d};
+ii comb_1(ii l, ii r) {
+    auto ans = vi{ l[0], l[1], r[0], r[1] };
     sort(ans.rbegin(), ans.rend());
-    return {ans[0], ans[1]};
+    return { ans[0], ans[1] };
+}
+
+/*
+    Um outro modo de fazer o mesmo, mas usando heap.. um pouco overkill, na minha opinião.
+*/
+
+ii comb_2(ii l, ii r) {
+    priority_queue<int> q;
+    q.push(l[0]);
+    q.push(l[1]);
+    q.push(r[0]);
+    q.push(r[1]);
+    int a = q.top(); q.pop();
+    int b = q.top(); q.pop();
+    return { a, b };
 }
 
 int main() {
     ii a, b;
-    a.first  = 10;
-    a.second = 20;
-    b.first  = 30;
-    b.second = 40;
-    auto answ = comb(a, b);
-    cout << answ.first << " " << answ.second << endl;
+    a[0] = 10;
+    a[1] = 20;
+    b[0] = 30;
+    b[1] = 40;
+    auto answ = comb_1(a, b);
+    cout << answ[0] << " " << answ[1] << endl;
     return 0;
 }
