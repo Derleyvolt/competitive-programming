fb7581e1e1da6ca917d139458af9e06d94641560
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2021-01-04 11:54:45 -0300

    Update ler_até_newline.cpp

--- ler_até_newline.cpp
+++ ler_até_newline.cpp
@@ -2,17 +2,29 @@
 
 using namespace std; 
 
+void example(string s = "1 2 3 4 5 6 7 8 9 10") {
+	stringstream str(s);
+    // str << s; atribuindo s à str
+	int num;
+	while (str >> num) // >> sobrecarregando pra inteiro
+		cout << num << endl;
+}
+
+void example_2(string s = "Eu juro solenemente nao fazer nada de bom!") {
+	stringstream str(s);
+	string word;
+    int cnt = 0;
+	while (str >> word) { // >> sobrecarregando pra strings
+        cout << num << endl;
+        cnt++; // contando o número de palavras..
+    }
+}
+
 int main() {
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
-    freopen("in.txt", "r", stdin);
-    // int b; cin >> b;  // 
-    // cin.ignore();     // necessário onde será lido um input da maneira usual e depois usarmos getline..
+    // int b; cin >> b; 
+    // cin.ignore(); necessário onde será lido um input da maneira usual e depois usarmos getline
     string line;
-    getline(cin,line);       // ler todos os caracteres do user input buffer no cin
-    istringstream iss(line); // essa classe se encarrega de me passar os dados
-    int a;
-    while(iss >> a) // passa os dados para a variável a, o >> é sobrecarregado pro tipo inteiro..
-        cout << a << endl;
+    getline(cin,line);  // ler o cin-buffer até o newline character e joga em line.
+    // a partir daqui usando o stringstream parser
     return 0;
 }
