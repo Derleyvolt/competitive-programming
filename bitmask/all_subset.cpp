1a8279d7c7e2e462e9ceca007b53366d511bbe78
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2021-01-07 20:59:40 -0300

    Create all_subset.cpp

---
+++ bitmask/all_subset.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> arr = { 1, 2, 3, 4, 5 };
+
+void subsets(int n = arr.size()) {
+    for (int i = 0; i < (1 << n); i++) {
+        // pegando apenas os subconjuntos com 3 elementos
+        //if (__builtin_popcount(i) != 3)
+        //    continue;
+        int cnt = 0;
+        if (i)
+            cout << "[";
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                if (cnt and j != n - 1)
+                    cout << ", ";
+                cout << arr[j];
+                cnt++;
+            }
+        }
+        if (i)
+            cout << "]" << endl;
+    }
+}
+
+int main() {
+    subsets();
+    return 0;
+}
