c3d987c23bd8d0351b1293ba05cf2c6508dcb551
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2021-01-07 13:34:18 -0300

    Create generate_all_divisors.cpp

---
+++ Teoria dos números/generate_all_divisors.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+vector<ll> divisors(ll n) {
+    vector<ll> ans;
+    for(ll i = 1; i*i <= n; i++) {
+        if(n % i == 0) {
+            ans.push_back(i);
+            if(n / i != i)
+                ans.push_back(n/i);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n; cin >> n;
+    auto arr = divisors(n);
+    for(auto a:arr) cout << a << endl;
+    return 0;
+}
