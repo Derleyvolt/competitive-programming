1969a50ff76cd17d81fe42408efa3afb756e4994
Mochileiro Ufológico <35679266+Derleyvolt@users.noreply.github.com>
2021-01-08 17:38:30 -0300

    Create binary_exponentiation.cpp

---
+++ Teoria dos números/binary_exponentiation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define m 1000000007
+#define ll long long
+
+ll exp(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll mod_exp(ll a, ll b) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m; // garantir que m * m não cause overflow, pois a * a poderiam, potencialmente, causar overflow.
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    binpow(8, 13);
+}
