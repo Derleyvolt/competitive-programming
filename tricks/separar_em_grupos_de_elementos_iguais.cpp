817bbb1bddbd0eb89c360dac1bd91c46c4cc5cf3
Wanderley <100Palavr@gmail.com>
2021-01-12 16:58:43 -0300

    Update separar_em_grupos_de_elementos_iguais.cpp

--- tricks/separar_em_grupos_de_elementos_iguais.cpp
+++ tricks/separar_em_grupos_de_elementos_iguais.cpp
@@ -2,25 +2,22 @@
 
 using namespace std;
 
-void separate(string& s) {
-    string ans;
+vector<string> separate(string& s) {
+    vector<string> ans(1);
     char c = s.front();
     for(auto a:s) {
         if(a == c)
-            ans.push_back(a);
-        else {
-            cout << ans << endl;
-            ans.clear();
-            c = a;
-            ans.push_back(a);
-        }
+            ans.back().push_back(a);
+        else
+            c = a, ans.push_back({a});
     }
-    cout << ans << endl; // os couts só são chamados no else do for-range, porém, quando estamos processando o último grupo de itens o bloco do else nunca será chamado
-                         // outro cenário seria onde todos os itens são iguais.. o bloco do else nunca seria chamado. 
+    
+    for(auto a:ans) cout << a << endl;    
+    return ans;
 }
 
 int main() {
-    string s; // AAABBBCCCCDDDDEEFFGHIJJJJJKKKKLLLEEERRRGGGHHHAAABBCCC
+    string s = "AAABBBCCCCDDDDEEFFGHIJJJJJKKKKLLLEEERRRGGGHHHAAABBCCC";
     separate(s);
     return 0;
 }
