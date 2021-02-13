9a14960f8c533ca4e83e36a0bd26b7fea43bfe84
Wanderley <100Palavr@gmail.com>
2021-02-13 10:19:28 -0300

    Update rotate_number.cpp

--- tricks/rotate_number.cpp
+++ tricks/rotate_number.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+// usando aritmética modular
 void rotate(int n) {
+     string s = to_string(n);
+     string aux = s;
+     for(int i = 0; i < s.size(); i++) {
+          for(int j = 0; j < s.size(); j++) {
+               aux[(i+j) % s.size()] = s[j];
+          }
+          cout << aux << endl;
+     }
+}
+
+
+// usando insert
+void rotate_ex(int n) {
     string s = to_string(n);
     cout << s << endl;
     for(int i = 1; i < s.size(); i++) {
@@ -13,5 +27,5 @@ void rotate(int n) {
 
 int main() {
     int n; cin >> n;
-    rotate(n);
+    rotate_ex(n);
 }
