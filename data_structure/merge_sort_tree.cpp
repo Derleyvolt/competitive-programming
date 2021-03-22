67cd8ac7cae8ca9cb6257278d7c43cbb4206b43b
Wanderley <100Palavr@gmail.com>
2021-03-22 05:07:28 -0300

    Update merge_sort_tree.cpp

--- data_structure/merge_sort_tree.cpp
+++ data_structure/merge_sort_tree.cpp
@@ -27,26 +27,6 @@ void build(int i, int l, int r) {
     merge(a.begin(), a.end(), b.begin(), b.end(), seg[i]);
 }
 
-void update(int i, int l, int r, int idx, int val) {
-    if(l == r) {
-        // é uma folha, é garantido ter no máx/min 1 elemento.
-        seg[i][0] = val;
-        return;
-    }
-
-    int m = (l+r)/2;
-
-    if(idx <= m)
-        update(i*2, l, m, idx, val);
-    else
-        update(i*2+1, m+1, r, idx, val);
-    
-    vi& a = seg[i * 2];
-    vi& b = seg[i * 2 + 1];
-
-    merge(a.begin(), a.end(), b.begin(), b.end(), seg[i]);
-}
-
 // void query(..) {...};
 // a função de consulta é muito dependente do problema..
 
