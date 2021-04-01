7cbf940227da0ed46d78d4947261bbd07cdf8762
Wanderley <100Palavr@gmail.com>
2021-04-01 02:57:10 -0300

    Update binary_exponentiation.cpp

--- Teoria dos números/binary_exponentiation.cpp
+++ Teoria dos números/binary_exponentiation.cpp
@@ -5,31 +5,31 @@ using namespace std;
 #define m 1000000007
 #define ll long long
 
-ll exp(ll a, ll b) {
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
+ll exp(ll base, ll e) {
+    ll ans = 1;
+    while (e > 0) {
+        if (e & 1)
+            ans = ans * base;
+        base = base * base;
+        e >>= 1;
     }
-    return res;
+    return ans;
 }
 
 // exponenciação binária modulada
 
-ll mod_exp(ll a, ll b) {
-    a %= m;
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m; // garantir que m * m não cause overflow, pois a * a poderiam, potencialmente, causar overflow.
-        b >>= 1;
+ll mod_exp(ll base, ll e) {
+    base %= m;
+    ll ans = 1;
+    while (e > 0) {
+        if (e & 1)
+            ans = ans * base % m;
+        base = base * base % m; // garantir que m * m não cause overflow, pois a * a poderiam, potencialmente, causar overflow.
+        e >>= 1;
     }
-    return res;
+    return ans;
 }
 
 int main() {
-    binpow(8, 13);
+    exp(8, 13);
 }
