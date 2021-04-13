26d22926c692a39c697be6406ddc3868b387d924
Wanderley <100Palavr@gmail.com>
2021-04-13 19:53:30 -0300

    Update count_inversions_fenwick_tree.cpp

--- misc/count_inversions_fenwick_tree.cpp
+++ misc/count_inversions_fenwick_tree.cpp
@@ -12,13 +12,13 @@ const int maxn = 1e7 + 10;
 int BIT[maxn], len;
 
 void update(int i, int val) {
-    for(;i<=len; i += i&(-i))
+    for(++i;i<=len; i += i&(-i))
         BIT[i] += val;
 }
 
 int query(int i) {
     int ans = 0;
-    for(;i>0; i-=i&(-i))
+    for(++i;i>0; i-=i&(-i))
         ans += BIT[i];
     return ans;
 }
@@ -32,10 +32,11 @@ int main() {
     vi arr(n);
     for(auto& a:arr) {
         cin >> a;
-        //a++; se array admitir o valor 0..
         len = max(len, a);
     }
 
+    BIT.resize(len+10);
+    
     int ans = 0;
     for(int i = n-1; i >= 0; i--) {
         ans += query(arr[i]-1); // olho quantos caras vieram antes de arr[i]
