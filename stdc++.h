6c2dab6855a18d2e8c204e8ab9d43fe1ce51b210
Wanderley <100Palavr@gmail.com>
2021-04-20 01:54:22 -0300

    Create stdc++.h

---
+++ stdc++.h
@@ -0,0 +1,77 @@
+// Este é o arquivo stdc++.h do compilador gcc.. eu tirei vários files desnecessários para a vida de um programador competitivo
+// portanto há aqui somente o necessário.
+
+// C:\MinGW\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits
+
+#include <assert.h>
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <streambuf>
+#include <string>
+#include <utility>
+#include <vector>
+#include <array>
+#include <chrono>
+#include <forward_list>
+#include <initializer_list>
+#include <random>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+// Macros e funções definidas por mim..
+
+using namespace std;
+
+mt19937 gen(time(NULL));
+
+int RNG(int min, int max) {
+    uniform_int_distribution<int> generator(min, max);
+    return generator(gen);
+}
+
+void db(const char* name, int val) {
+    cout << name << " -> " << val << endl;
+}
+
+void db(int val, const char* name) {
+    cout << name << " -> " << val << endl;
+}
+
+// pode ser passado qualquer container que tenha begin e end como membros e que tenha 
+// um tipo aceito pelo <<operator do cout como argumento.
+template<class vet>
+void db(vet arr, const char* name) {
+    cout << name << " -> [ ";
+    for(auto a : arr) cout << a << ", ";
+    cout << "]" << endl;
+}
+
+#define NAME(v) #v
+#define db(arg) db(arg, NAME(arg))
+
+
