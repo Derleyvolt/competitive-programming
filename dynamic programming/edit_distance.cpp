8695ea23e797de3f395d28a1d67338e9b631899a
Wanderley <100Palavr@gmail.com>
2021-06-07 05:51:17 -0300

    Update edit_distance.cpp

--- dynamic programming/edit_distance.cpp
+++ dynamic programming/edit_distance.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string s1, s2;
+
+int solve(int i, int j) {
+    // s1 > s2, então eu removo (s1.size()-i) caracteres
+    if(j >= s2.size())
+        return (s1.size()-i);
+  
+    // s2 > s1, então eu removo (s2.size()-j) caracteres    
+    if(i >= s1.size())
+        return (s2.size()-j);
+
+    if(s1[i] == s2[j])
+        return solve(i+1, j+1);
+    
+    // remove, replace e insert, respectivamente.
+    return min({solve(i+1, j), solve(i+1, j+1), solve(i, j+1)}) + 1;
+}
+
+int main() {
+    s1 = "intention";
+    s2 = "execution";
+    cout << solve(0, 0) << endl;
+    return 0;
+}
