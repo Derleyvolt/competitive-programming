a5ff2932055646f1203559ce10e48b164c8670d7
Wanderley <100Palavr@gmail.com>
2021-06-28 06:49:51 -0300

    Create binomial_coefficient.cpp

---
+++ dynamic programming/binomial_coefficient.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1000][1000];
+
+// O(n*k)
+int solve(int n, int k) {
+    if(k == 0 or k == n)
+        return 1;
+    if(n == 2)
+        return 2;
+
+    if(dp[n][k] != -1)
+        return dp[n][k];
+    return dp[n][k] = solve(n-1, k-1) + solve(n-1, k); // relação de stifel
+}
+
+int main() {
+    memset(dp, -1, sizeof dp);
+    cout << solve(17, 7) << endl;
+    return 0;
+}
