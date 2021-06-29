309f52af577f8c69d0529a30317da0614622dfa3
Wanderley <100Palavr@gmail.com>
2021-06-29 19:22:34 -0300

    Update modular_multiplicative_inverse.cpp

--- Teoria dos números/modular_multiplicative_inverse.cpp
+++ Teoria dos números/modular_multiplicative_inverse.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+#define ll  long long
+
+const ll m = 1e9 + 7;
+
+// basea-se no pequeno teorema de fermat, portanto m precisa ser primo.
+
+ll expmod(ll b, ll e) {
+    ll ans = 1;
+    while(e > 0) {
+        if(e&1) {
+            ans = (ans * b) % m;
+        }
+
+        b = (b * b) % m;
+        e = e >> 1;
+    }
+
+    return ans;
+}
+
+int main() {
+    cout << expmod(156156, m-2) << endl;
+    return 0;
+}
