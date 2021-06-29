17539848b1422498c15b2724dff857504f76a335
Wanderley <100Palavr@gmail.com>
2021-06-29 19:29:48 -0300

    Update modular_multiplicative_inverse.cpp

--- Teoria dos números/modular_multiplicative_inverse.cpp
+++ Teoria dos números/modular_multiplicative_inverse.cpp
@@ -8,6 +8,8 @@ const ll m = 1e9 + 7;
 
 // basea-se no pequeno teorema de fermat, portanto m precisa ser primo.
 
+// https://cp-algorithms.com/algebra/module-inverse.html
+
 ll expmod(ll b, ll e) {
     ll ans = 1;
     while(e > 0) {
